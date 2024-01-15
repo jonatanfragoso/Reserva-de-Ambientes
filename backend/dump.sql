a7841eab7d7ef6fbd430abce29f3c7ee3516dbcc
Jonatan Fragoso <jonatanfragoso@gmail.com>
2024-01-15 09:42:37 -0500

    subindo arquivo dump.sql

---
+++ backend/dump.sql
@@ -0,0 +1,38 @@
+create table "funcao" (
+	id serial primary key,
+  descricao varchar(50)
+);
+
+create table "gestor" (
+	id serial primary key,
+  descricao varchar(50)
+);
+
+create table "usuarios" (
+	id serial primary key,
+  nome varchar(255) not null,
+  email varchar(255) unique not null,
+  telefone varchar(100) not null,
+  matricula varchar(50) not null,
+  id_funcao integer not null,
+  id_gestor integer not null,
+  foreign key (id_funcao) references funcao (id),
+  foreign key (id_gestor) references gestor (id)
+);
+
+create table "locais" (
+	id serial primary key,
+  descricao varchar(100)
+);
+
+create table "agendamentos" (
+	id serial primary key,
+  hora time not null,
+  data_agendamento date not null,
+  id_usuario integer not null,
+  id_gestor integer not null,
+  id_local integer not null,
+  foreign key (id_usuario) references usuarios (id),
+  foreign key (id_gestor) references gestor (id),
+  foreign key (id_local) references locais (id)
+);
